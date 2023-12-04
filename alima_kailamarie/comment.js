8df3e52fd9873b3211ec2675c779a045c907e88f
Alima, Kaila Marie <146612718+alima-kailamarie@users.noreply.github.com>
2023-12-04 21:13:06 +0800

    modify the code in comment.js

--- alima_kailamarie/comment.js
+++ alima_kailamarie/comment.js
@@ -35,25 +35,36 @@ commentButton.disabled = true;
 });
 });
 
-$("#searchBtn").click(function() {
-    let country = $("#countrySearch").val();
-    let region = "";
-
-$.getJSON("https://restcountries.com/v3.1/name/" + country, function(data) {
-    $("#countryDetails").html("<h2>" + data.name.common + "</h2><p>Capital: " 
-        + data.capital + "</p><p>Population: " + data.population + "</p>");
-        region = data.region;
-});
-
-$.getJSON("https://restcountries.com/v3.1/region/" + region, function(data) {
-    let relatedCountriesHTML = "<h3>Other countries in " + region + 
-        ":</h3><ul>";
-
-    for (let i = 0; i < data.length; i++) {
-        relatedCountriesHTML += "<li>" + data[i].name.common + "</li>";
-    }
+async function searchCountry() {
+const countryInput = document.getElementById('countryInput').value;
     
-    relatedCountriesHTML += "</ul>";
-    $("#relatedCountries").html(relatedCountriesHTML);
-    });
+const countryResponse = await 
+    fetch(`https://restcountries.com/v3.1/name/${countryInput}`);
+const countryData = await countryResponse.json();
+    
+if (countryData.length > 0) {
+    const countryDetails = document.getElementById('countryDetails');
+    countryDetails.innerHTML = `<h2>${countryData[0].name.common}</h2>
+                                <p>Region: ${countryData[0].region}</p>
+                                <p>Capital: ${countryData[0].capital}</p>
+                                <p>Population: ${countryData[0].population}</p>
+                                <p>Area: ${countryData[0].area} sq km</p>
+                                <p>Language: ${Object.values(countryData[0]
+                                    .languages).join(', ')}</p>`;
+        
+const regionResponse = await 
+    fetch(`https://restcountries.com/v3.1/region/${countryData[0].region}`);
+const regionData = await regionResponse.json();
+        
+const countriesInRegion = document.getElementById('countriesInRegion');
+countriesInRegion.innerHTML = '<h2>Other Countries in the Same Region</h2>';
+        
+regionData.forEach(country => {
+    if (country.name.common !== countryData[0].name.common) {
+        countriesInRegion.innerHTML += <p>${country.name.common}</p>;
+    }
 });
+    } else {
+        alert('Country not found. Please enter a valid country name.');
+    }
+}
